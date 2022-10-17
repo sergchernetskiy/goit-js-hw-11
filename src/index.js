5c258b0a38f7ebfe8101bb6becb1c33628eed4ec
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-10-17 21:59:01 +0300

    lightbox fixed

--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const pixabay = new pixabayAPI();
 //console.log(pixabay);
+const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
 
 refs.form.addEventListener('submit', onSubmit);
-
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onSubmit(event) {
   event.preventDefault();
@@ -48,21 +52,17 @@ async function onSubmit(event) {
     pixabay.calculateTotalPages(total);
     Notify.success(`${total} images found`);
     refs.form.reset();
-    const lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
+    lightbox.refresh();
 
     if (pixabay.isShowLoadMore) {
       refs.loadMoreBtn.classList.remove('is-hidden');
+      onLoadMore();
     }
     onSpinnerStop();
-    refs.loadMoreBtn.addEventListener('click', onLoadMore);
   } catch (error) {Notify.failure(error.message, 'Request error');
     clearPage();
 }
-
-
+  
 function onLoadMore() {
   pixabay.increment();
   if (!pixabay.isShowLoadMore) {
@@ -75,11 +75,7 @@ function onLoadMore() {
     .getPhotos()
     .then(({ hits }) => {
       createMarkup(hits);
-      const gallery = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      gallery.refresh();
+      lightbox.refresh();
     })
     .catch(error => {
       Notify.failure(error.message, 'Request error');
@@ -89,90 +85,6 @@ function onLoadMore() {
 
 function clearPage() {
   pixabay.resetPage();
-  refs.list.innerHTML = '';
   refs.loadMoreBtn.classList.add('is-hidden');
-}
-
-///////////////////////////////////////////////
-/* function onSubmit(event) {
-  event.preventDefault();
-
-  onSpinnerStart();
-
-  const {
-    elements: { searchQuery },
-  } = event.currentTarget;
-  const request = searchQuery.value.trim().toLowerCase();
-  console.log(request);
-
-  if (!request) {
-    Notify.failure('Enter the search data');
-    onSpinnerStop();
-    return;
-  }
-
-  pixabay.request = request;
-  clearPage();
-
-  pixabay
-    .getPhotos()
-    .then(({ hits, total }) => {
-      if (hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-
-      createMarkup(hits);
-      //console.log(hits);
-      pixabay.calculateTotalPages(total);
-      Notify.success(`${total} images found`);
-      refs.form.reset();
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-
-      if (pixabay.isShowLoadMore) {
-        refs.loadMoreBtn.classList.remove('is-hidden');
-      }
-    })
-    .catch(error => {
-      Notify.failure(error.message, 'Request error');
-      clearPage();
-    })
-    .finally(() => {
-      onSpinnerStop();
-    });
-}
-
-function onLoadMore() {
-  pixabay.increment();
-  if (!pixabay.isShowLoadMore) {
-    refs.loadMoreBtn.classList.add('is-hidden');
-    Notify.info(
-      'We are sorry, but you have reached the end of search results.'
-    );
-  }
-  pixabay
-    .getPhotos()
-    .then(({ hits }) => {
-      createMarkup(hits);
-      const gallery = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      gallery.refresh();
-    })
-    .catch(error => {
-      Notify.failure(error.message, 'Request error');
-      clearPage();
-    });
-}
-
-function clearPage() {
-  pixabay.resetPage();
   refs.list.innerHTML = '';
-  refs.loadMoreBtn.classList.add('is-hidden');
-} */
+}
